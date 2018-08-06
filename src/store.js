a9c2a4dfa92b8a3bc3660f8a4edefbf4b0b28e30
Kevin Herklotz <scheek.hee@gmail.com>
2018-08-06 19:28:21 +0200

    added Chrome extension support

--- src/store.js
+++ src/store.js
@@ -1,14 +1,20 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
+/* eslint-disable no-underscore-dangle */
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+/* eslint-enable */
+
 // reducer(Function), [preloadedState](any), [enhancer](Function)
 const store = createStore(
   rootReducer,
   initialState,
-  applyMiddleware(...middleware),
+  composeEnhancers(
+    applyMiddleware(...middleware),
+  ),
 );
 
 export default store;
